a259c13527076100f2ee69b985fd3fba674be223
kesh4207 <keseanarobinson1@outlook.com>
2020-05-19 19:39:53 +0100

    Adding navigation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const slider= document.querySelector('.slider');
 const logo= document.querySelector('#logo');
 const headline= document.querySelector('headline');
 const tl = new TimelineMax();
-tl.fromTo(hero,1,{height:"0%"}, {height:"80%", ease: Power2.easeInOut})
+tl.fromTo(hero,1.5,{height:"0%"}, {height:"90%", ease: Power2.easeInOut})
 .fromTo(hero,1.5,{width:"80%"}, {width:"100%", ease: Power2.easeInOut });
 
 
